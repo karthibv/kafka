99337f973daf6062f881a1aeb58f484c46ab6d44
Karthik <karthik@karthiks-mbp.lan>
2018-04-01 17:47:20 +0800

    spring boot kafka sample  commit

---
+++ springboot-kafka-example/src/main/java/com/kafka/example/SpringbootKafkaExampleApplication.java
@@ -0,0 +1,12 @@
+package com.kafka.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaExampleApplication.class, args);
+	}
+}
